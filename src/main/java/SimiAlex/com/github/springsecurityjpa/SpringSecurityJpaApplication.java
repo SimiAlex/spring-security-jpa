2c72c4583070bfd286f7cbab6712bb043c1dd4f0
SimiAlex <simineaalexandru@gmail.com>
2021-09-07 20:33:55 +0300

    Initial commit

---
+++ src/main/java/SimiAlex/com/github/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package SimiAlex.com.github.springsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
